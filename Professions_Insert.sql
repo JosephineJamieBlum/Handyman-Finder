2350e8b60a33e491245081bd28783ee3c6993537
Tazz Akber <90526357+butteredtoasts@users.noreply.github.com>
2021-10-25 16:02:44 -0400

    Add files via upload
    Query for Professions table insert

---
+++ Professions_Insert.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Professions
+(
+Plumber varchar(20),
+Electrician varchar(20),
+Carpenter varchar(20),
+Painter varchar(20),
+HVAC varchar(20),
+Landscaper varchar(20),
+Appliance_Installation varchar(20)
+);
